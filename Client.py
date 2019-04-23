01da8168f286d8486d89fb76466b37cb62fc8df6
Meran Barghouthi <meran_97@yahoo.com>
2019-04-23 22:20:31 +0300

    Modify the code to accept ls

--- Client.py
+++ Client.py
@@ -1,8 +1,8 @@
-# from socket import *
-# serverAddress = ("82.205.79.78",12000) # a tuple containing the IP Address of the Server and Socket ID (Port)
-# clientSocket = socket(AF_INET,SOCK_DGRAM)# Datagram because we're using UDP
-# message = input("input lower case sentence: ")
-# clientSocket.sendto(message.encode(),serverAddress)
-# modifiedMessage,serverAddress = clientSocket.recvfrom(2048)
-# print(modifiedMessage.decode())
-# clientSocket.close()
+from socket import *
+serverAddress = ("192.168.1.112",12000) # a tuple containing the IP Address of the Server and Socket ID (Port)
+clientSocket = socket(AF_INET,SOCK_DGRAM)# Datagram because we're using UDP
+message = input("Input command: ")
+clientSocket.sendto(message.encode(),serverAddress)
+modifiedMessage,serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage)
+clientSocket.close()
--- Server.py
+++ Server.py
@@ -1,15 +1,18 @@
-# from socket import *
-# serverAddress = ("25.66.140.253",12000) # a tuple containing the IP Address of the Server and Socket ID (Port)
-# serverSocket = socket(AF_INET,SOCK_DGRAM) # Datagram because we're using UDP
-# serverSocket.bind(serverAddress)
-# print('starting up on %s port %s' % serverAddress)
-# while True:
-#     message, clientAddress = serverSocket.recvfrom(2048)
-#     modifiedMessasge = message.decode().upper()
-#     if modifiedMessasge:
-#         print(modifiedMessasge+" from %s port %s"%clientAddress)
-#     serverSocket.sendto(modifiedMessasge.encode(),clientAddress)
-
+from socket import *
 from os import listdir
 from os.path import isfile, join
-onlyfiles = [f for f in listdir('.') if isfile(join('.', f))]
+
+serverAddress = ("192.168.1.112",12000) # a tuple containing the IP Address of the Server and Socket ID (Port)
+serverSocket = socket(AF_INET,SOCK_DGRAM) # Datagram because we're using UDP
+serverSocket.bind(serverAddress)
+print('starting up on %s port %s' % serverAddress) # debug
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)     
+    modifiedMessasge = message.decode()
+    if modifiedMessasge is 'ls':
+        print(modifiedMessasge+" from %s port %s"%clientAddress) # debug
+        onlyfiles = [f for f in listdir('.') if isfile(join('.', f))]
+        serverSocket.sendto(str(onlyfiles),clientAddress)
+    else:
+        serverSocket.sendto("Incorrect command",clientAddress)
+
