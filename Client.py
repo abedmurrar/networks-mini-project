71f40646d5f95ff0ab3314d54cc54b6dd6e94185
Abed Murrar <abedmurrar15@gmail.com>
2019-04-23 22:43:42 +0300

    continue client sequence

--- Client.py
+++ Client.py
@@ -1,8 +1,12 @@
 from socket import *
 serverAddress = ("192.168.1.112",12000) # a tuple containing the IP Address of the Server and Socket ID (Port)
 clientSocket = socket(AF_INET,SOCK_DGRAM)# Datagram because we're using UDP
-message = input("Input command: ")
-clientSocket.sendto(message.encode(),serverAddress)
+command = input("Input command: ")
+clientSocket.sendto(command.encode(),serverAddress)
 modifiedMessage,serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage)
+file_name = input("Which file do you want? ")
+clientSocket.sendto(file_name.encode(),serverAddress)
+file_content = clientSocket.recvfrom(2048)
+print(file_content)
 clientSocket.close()
