90adf80f2973561c4b32a6b95d0cb210b6415ccc
Abed Murrar <abedmurrar15@gmail.com>
2019-04-23 22:43:28 +0300

    update accepted commands to accept file name

--- Server.py
+++ Server.py
@@ -2,17 +2,24 @@ from socket import *
 from os import listdir
 from os.path import isfile, join
 
-serverAddress = ("192.168.1.112",12000) # a tuple containing the IP Address of the Server and Socket ID (Port)
+
+onlyfiles = [f for f in listdir('./files') if isfile(join('./files', f))]
+
+serverAddress = ("192.168.1.111",12000) # a tuple containing the IP Address of the Server and Socket ID (Port)
 serverSocket = socket(AF_INET,SOCK_DGRAM) # Datagram because we're using UDP
 serverSocket.bind(serverAddress)
 print('starting up on %s port %s' % serverAddress) # debug
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)     
+    message, clientAddress = serverSocket.recvfrom(2048) 
     modifiedMessasge = message.decode()
-    if modifiedMessasge is 'ls':
-        print(modifiedMessasge+" from %s port %s"%clientAddress) # debug
-        onlyfiles = [f for f in listdir('.') if isfile(join('.', f))]
+    print(modifiedMessasge+" from %s port %s"%clientAddress) # debug
+    if modifiedMessasge == 'ls':
         serverSocket.sendto(str(onlyfiles),clientAddress)
+    elif modifiedMessasge in onlyfiles:
+        data=''
+        with open('./files/'+modifiedMessasge, 'r') as file:
+            data = file.read()
+        serverSocket.sendto(data,clientAddress)
     else:
         serverSocket.sendto("Incorrect command",clientAddress)
 
