117d03cbb56ef56284878ca9ece7aff56f9e8a82
Meran Barghouthi <meran_97@yahoo.com>
2019-04-30 15:27:55 +0300

    update messages, change ip to localhost

--- Server.py
+++ Server.py
@@ -2,10 +2,9 @@ from socket import *
 from os import listdir
 from os.path import isfile, join
 
-
 onlyfiles = [f for f in listdir('./files') if isfile(join('./files', f))]
 
-serverAddress = ("192.168.1.111",12000) # a tuple containing the IP Address of the Server and Socket ID (Port)
+serverAddress = ("127.0.0.1",12000) # a tuple containing the IP Address of the Server and Socket ID (Port)
 serverSocket = socket(AF_INET,SOCK_DGRAM) # Datagram because we're using UDP
 serverSocket.bind(serverAddress)
 print('starting up on %s port %s' % serverAddress) # debug
@@ -14,13 +13,13 @@ while True:
     modifiedMessasge = message.decode()
     print(modifiedMessasge+" from %s port %s"%clientAddress) # debug
     if modifiedMessasge == 'ls':
-        serverSocket.sendto(str(onlyfiles).encode(),clientAddress)
+        serverSocket.sendto("\n".join(onlyfiles).encode(),clientAddress)
     elif modifiedMessasge in onlyfiles:
         data=''
         with open('./files/'+modifiedMessasge, 'r') as file:
             data = file.read()
         serverSocket.sendto(data.encode(),clientAddress)
     else:
-        serverSocket.sendto("Incorrect command".encode(),clientAddress)
+        serverSocket.sendto("File not found".encode(),clientAddress)
 
     modifiedMessasge = message
